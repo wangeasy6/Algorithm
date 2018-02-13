079daa1752a36e8bc0d1e652bc3f49548238b5c2
wangeasy6 <1055048604@qq.com>
2018-02-13 23:18:17 +0800

    修复一个BUG，多路归并实现

--- 100G_Txt_Sort/sort_text.c
+++ 100G_Txt_Sort/sort_text.c
@@ -1,33 +1,33 @@
 /*
 *  使用了 200M 的文件，内存限制在 4M， 模拟100G文件的排序
-*  约在50s左右
+*  约在20s左右
 *  Coder：yirong.wang
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define ORG_FILE_NAME "100G.txt"
-#define OUT_FILE_NAME "100G_Sorted.txt"
-#define TMP_FILE_NAME_PRE "tmp_%c_%d.txt"
-#define FILE_NAME_LEN 128
-#define MEM_SIZE (4 * 1024 * 1024)
-#define TXT_LINE_LEN 1024
-#define MAX_LINE (MEM_SIZE/TXT_LINE_LEN)
-#define CMD_MAX_LEN 256
+#define FILE_NAME_LEN 256                       //文件名最大长度
+#define CMD_MAX_LEN  256                        //System 命令最大长度
+#define ORG_FILE_NAME "100G.txt"                //原文件名
+#define OUT_FILE_NAME "100G_Sorted.txt"         //输出文件名
+#define TMP_FILE_NAME_PRE "tmp_%d_%d.txt"       //临时文件名
+#define MEM_SIZE (4 * 1024 * 1024)              //使用内存 4M
+#define TXT_LINE_LEN 1024                       //每一行文本长度
+#define MAX_LINE (MEM_SIZE/TXT_LINE_LEN)        //内存/文本长度 = 行数
+#define MULT_MIX_N 6                            //多路合并数
 
 int main(void)
 {
       int i,j;
       FILE *fd_org = NULL;
-      FILE *fd_a = NULL, *fd_b = NULL, *fd_o = NULL;
+      FILE *fd_i[MULT_MIX_N], *fd_o = NULL;
       char **tmp_buf = NULL;
-      char file_sort_count = 'a', upper = 'o';
-      int read_len, count_tmp = 0, count_file = 0, all_tmp_file = 0;
+      int file_sort_count = 0, min = -1;
+      int read_len, count_tmp = 0, count_file = 0, count_file_end = 0, all_tmp_file = 0;
       int sub_sort[MAX_LINE] = {0},tmp_t = 0;
       char mv_cmd[CMD_MAX_LEN] = {0}, rm_cmd[CMD_MAX_LEN] = {0};
-      char tmp_buf_a[TXT_LINE_LEN] = {0}, tmp_buf_b[TXT_LINE_LEN] = {0};
-      char file_name_o[FILE_NAME_LEN] = {0},file_name_a[FILE_NAME_LEN] = {0},file_name_b[FILE_NAME_LEN] = {0};
+      char file_name_o[FILE_NAME_LEN] = {0},file_name_i[MULT_MIX_N][FILE_NAME_LEN] = {0};
 
       tmp_buf = (char**)malloc(sizeof(char*) * MAX_LINE);         //申请内存
       for(i = 0; i < MAX_LINE; i++)
@@ -38,7 +38,7 @@ int main(void)
       fd_org = fopen(ORG_FILE_NAME, "r");
       if(!fd_org)
       {
-            printf("Create File Failed, %s\n", ORG_FILE_NAME);
+            printf("Open File Failed, %s\n", ORG_FILE_NAME);
             return 0;
       }
       printf("file_org Open Success\n");
@@ -149,24 +149,26 @@ int main(void)
       {
             count_file = 0;
             file_sort_count++;
-            for(i = 0; i < all_tmp_file-1; i+=2)
+            for(i = 0; i < (all_tmp_file-1); i+=MULT_MIX_N)
             {
-                  snprintf(file_name_a, FILE_NAME_LEN, TMP_FILE_NAME_PRE, file_sort_count-1, i);       //合并输入
-                  fd_a = fopen(file_name_a, "r");
-                  if(!fd_a)
+                  count_file_end = (all_tmp_file - i)>MULT_MIX_N?MULT_MIX_N:(all_tmp_file - i);
+                  //printf("[Easy-W] %d %d %d\n", all_tmp_file, i, count_file_end);
+                  for(j = 0; j < count_file_end; j++)
                   {
-                        printf("Read File Failed, %s\n", file_name_a);
-                        return 0;
-                  }
-                  snprintf(file_name_b, FILE_NAME_LEN, TMP_FILE_NAME_PRE, file_sort_count-1, i+1);     //合并输入
-                  fd_b = fopen(file_name_b, "r");
-                  if(!fd_b)
-                  {
-                        printf("Read File Failed, %s\n", file_name_b);
-                        return 0;
+                        snprintf(file_name_i[j], FILE_NAME_LEN, TMP_FILE_NAME_PRE, file_sort_count-1, i+j);       //合并输入
+                        fd_i[j] = fopen(file_name_i[j], "r");
+                        if(!fd_i[j])
+                        {
+                              printf("Read File Failed, %s\n", file_name_i[j]);
+                              return 0;
+                        }
+                        else
+                        {
+                              printf("Read File %s\n", file_name_i[j]);
+                        }
                   }
 
-                  if(all_tmp_file == 2)
+                  if(all_tmp_file <= MULT_MIX_N)
                   {
                         strncpy(file_name_o, OUT_FILE_NAME, FILE_NAME_LEN);                      //最终输出
                   }
@@ -180,71 +182,58 @@ int main(void)
                         printf("Create File Failed, %s\n", file_name_o);
                         return 0;
                   }
+                  else
+                  {
+                        printf("Create File %s\n",file_name_o);
+                  }
 
                   do
                   {
-                        if(upper != 'a')
+                        for(j = 0; j < ((all_tmp_file - i)>MULT_MIX_N?MULT_MIX_N:(all_tmp_file - i)); j++)
                         {
-                              if(!feof(fd_a))
+                              if(min == -1 || min == j)
                               {
-                                    if(!fgets(tmp_buf_a, TXT_LINE_LEN, fd_a))
+                                    if(!fgets(tmp_buf[j], TXT_LINE_LEN, fd_i[j]))
                                     {
-                                          printf("Read %s File End\n", file_name_a);
+                                          count_file_end--;
+                                          strncpy(tmp_buf[j], "~", 2);
+                                          printf("Read %s File End,Last:%d\n", file_name_i[j],count_file_end);
                                           continue;
                                     }
                               }
-                              else
-                              {
-                                    strncpy(tmp_buf_a, "~", 2);
-                              }
                         }
+                        if (!count_file_end)
+                              break;
 
-                        if(upper != 'b')
+                        for(min = 0,j = 1; j < ((all_tmp_file - i)>MULT_MIX_N?MULT_MIX_N:(all_tmp_file - i)); j++)
                         {
-                              if(!feof(fd_b))
-                              {
-                                    if(!fgets(tmp_buf_b, TXT_LINE_LEN, fd_b))
-                                    {
-                                          printf("Read %s File End\n", file_name_b);
-                                          continue;
-                                    }
-                              }
-                              else
-                              {
-                                    strncpy(tmp_buf_b, "~", 2);
-                              }
+                              min = (strcmp(tmp_buf[min], tmp_buf[j])>0)?j:min;
                         }
+                        fwrite(tmp_buf[min], 1, strlen(tmp_buf[min]), fd_o);
+                  }while(1);
 
-                        if (strcmp(tmp_buf_a, tmp_buf_b) > 0)
-                        {
-                              upper = 'a';
-                              fwrite(tmp_buf_b, 1, strlen(tmp_buf_b), fd_o);
-                        }
-                        else
-                        {
-                              upper = 'b';
-                              fwrite(tmp_buf_a, 1, strlen(tmp_buf_a), fd_o);
-                        }
-                  }while(!feof(fd_a) || !feof(fd_b));
-
-                  upper = 'o';
-                  fclose(fd_a);
-                  fclose(fd_b);
+                  min = -1;
+                  for(j = 0; j < MULT_MIX_N; j++)
+                  {
+                        if(fd_i[j])
+                              fclose(fd_i[j]);
+                  }
                   fclose(fd_o);
-                  //delete a,b
                   count_file++;
             }
-            if(all_tmp_file > 1 && all_tmp_file%2 == 1)
+
+            if(all_tmp_file > 1 && all_tmp_file%MULT_MIX_N == 1)
             {
-                  snprintf(file_name_b, FILE_NAME_LEN, TMP_FILE_NAME_PRE, file_sort_count-1, i);
+                  snprintf(file_name_i[0], FILE_NAME_LEN, TMP_FILE_NAME_PRE, file_sort_count-1, i);
                   snprintf(file_name_o, FILE_NAME_LEN, TMP_FILE_NAME_PRE, file_sort_count, count_file);
-                  snprintf(mv_cmd, CMD_MAX_LEN, "mv %s %s", file_name_b, file_name_o);
+                  snprintf(mv_cmd, CMD_MAX_LEN, "rename %s %s", file_name_i[0], file_name_o);         //Winidows下的命令是 rename；Linux 下的命令是 mv
                   printf("CMD Run: %s\n", mv_cmd);
                   system(mv_cmd);
                   count_file++;
             }
 
-            snprintf(rm_cmd, CMD_MAX_LEN, "rm tmp_%c_*", file_sort_count-1);
+            snprintf(rm_cmd, CMD_MAX_LEN, "rm tmp_%d_*", file_sort_count-1);
+            printf("CMD Run: %s\n", rm_cmd);
             system(rm_cmd);
             all_tmp_file = count_file;
       }
